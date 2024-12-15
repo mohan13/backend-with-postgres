cef872d9701aba6268642d0f1beae683dad07d32
mohan13 <mohangrg250@gmail.com>
2024-12-15 09:29:03 +0545

    fixed auth middlware

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import User from "../db/models/userModel";
 import { ApiError } from "../utils/ApiError";
+import { asyncHandler } from "../utils/asyncHandler";
 
 export interface AuthRequest extends Request {
   user?: {
@@ -18,54 +19,92 @@ export enum Role {
   Customer = "customer",
 }
 
-class AuthController {
-  async isAuthenticated(
-    req: AuthRequest,
-    res: Response,
-    next: NextFunction
-  ): Promise<void> {
-    //get token from user
-    const token = req.cookies?.accessToken || req.headers.authorization;
-    if (!token || token === undefined) {
-      throw new ApiError(401, "Unauthorized request");
+// class AuthController {
+//   async isAuthenticated(
+//     req: AuthRequest,
+//     res: Response,
+//     next: NextFunction
+//   ): Promise<void> {
+//     //get token from user
+//     const token = req.cookies?.accessToken || req.headers.authorization;
+//     if (!token || token === undefined) {
+//       throw new ApiError(401, "Unauthorized request");
+//     }
+
+//     jwt.verify(
+//       token,
+//       process.env.SECRET_KEY as string,
+//       async (err: any, decoded: any) => {
+//         if (err) {
+//           throw new ApiError(401, "Invalid Token");
+//         } else {
+//           try {
+//             const userData = await User.findByPk(decoded.id);
+//             //check if that decoded object id user exist or not
+//             if (!userData) {
+//               throw new ApiError(404, "No user with that token");
+//             }
+//             req.user = userData;
+//             next();
+//           } catch (error: any) {
+//             throw new ApiError(500, error?.message || "Something went wrong");
+//           }
+//           res;
+//         }
+//       }
+//     );
+//     // verify token if it is legit or tampered
+//   }
+
+//   restrictTo(...roles: Role[]) {
+//     return (req: AuthRequest, res: Response, next: NextFunction) => {
+//       let userRole = req.user?.role as Role;
+//       if (!roles.includes(userRole)) {
+//         res.status(403).json({ message: "You don't have permission" });
+//       } else {
+//         next();
+//       }
+//     };
+//   }
+//   //   restrictTo(...roles: Role[]) {}
+// }
+
+// export default new AuthController();
+
+export const verifyJWT = asyncHandler(
+  async (req: AuthRequest, _: any, next: NextFunction) => {
+    try {
+      const token =
+        req.cookies?.token ||
+        req.header("Authorization")?.replace("Bearer ", "");
+      if (!token || token === undefined) {
+        throw new ApiError(401, "Unauthorized request");
+      }
+      const decodedToken: any = jwt.verify(
+        token,
+        process.env.SECRET_KEY as string
+      );
+      const user = await User.findByPk(decodedToken?.id);
+
+      if (!user) {
+        throw new ApiError(401, "Invalid Access Token");
+      }
+      req.user = user;
+
+      next();
+    } catch (error: any) {
+      throw new ApiError(401, error?.message || "Invalid access token");
     }
-
-    jwt.verify(
-      token,
-      process.env.SECRET_KEY as string,
-      async (err: any, decoded: any) => {
-        if (err) {
-          throw new ApiError(401, "Invalid Token");
-        } else {
-          try {
-            const userData = await User.findByPk(decoded.id);
-            //check if that decoded object id user exist or not
-            if (!userData) {
-              throw new ApiError(404, "No user with that token");
-            }
-            req.user = userData;
-            next();
-          } catch (error: any) {
-            throw new ApiError(500, error?.message || "Something went wrong");
-          }
-          res;
-        }
-      }
-    );
-    // verify token if it is legit or tampered
   }
+);
 
-  restrictTo(...roles: Role[]) {
-    return (req: AuthRequest, res: Response, next: NextFunction) => {
-      let userRole = req.user?.role as Role;
-      if (!roles.includes(userRole)) {
-        res.status(403).json({ message: "You don't have permission" });
-      } else {
-        next();
-      }
-    };
-  }
-  //   restrictTo(...roles: Role[]) {}
-}
-
-export default new AuthController();
+export const restrictTo = (...roles: Role[]) => {
+  return (req: AuthRequest, res: Response, next: NextFunction) => {
+    let userRole = req.user?.role as Role;
+    if (!roles.includes(userRole)) {
+      res.status(403).json({ message: "You don't have permission" });
+    } else {
+      next();
+    }
+  };
+};
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -6,19 +6,13 @@ import {
   registerUser,
 } from "../controller/user.controller";
 import { upload } from "../middleware/multer";
-import authMiddleware, { Role } from "../middleware/authMiddleware";
+import { restrictTo, Role, verifyJWT } from "../middleware/authMiddleware";
 
 const router: Router = express.Router();
 
 router.route("/register").post(upload.single("userImage"), registerUser);
 router.route("/login").post(loginUser);
 router.route("/logout").post(logoutUser);
-router
-  .route("/userProfile")
-  .get(
-    authMiddleware.isAuthenticated,
-    authMiddleware.restrictTo(Role.Customer),
-    getUser
-  );
+router.route("/profile").get(verifyJWT, restrictTo(Role.Customer), getUser);
 
 export default router;
